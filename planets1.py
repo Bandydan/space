f825c997b605d02ad2eb02f3fabcdb7f33e083e8
Daniil Marynich <bandydan@gmail.com>
2017-02-11 14:36:26 +0200

    our first space program

---
+++ planets1.py
@@ -0,0 +1,83 @@
+planets = {
+    '1':{
+        'Name': 'Mercury',
+        'Distance': 58*(10**6),
+        'Satellite': [] #{'Name':'ups'},
+        'Weight': 3,33022*(10**23), #kg
+        'The first cosmic speed': 3,1 #km/s,
+        'The second cosmic speed': 4,25 #km/s,
+        'Average radius': 2439,7 #km
+    },
+    '2':{
+        'Name': 'Venera',
+        'Distance': 108*(10**6),
+        'Satellite': [] #{'Name':'ups'},
+        'Weight': 4,8675*(10**24) #kg
+        'The first cosmic speed': 7,328 #km/s
+        'The second cosmic speed': 10,363 #km/s
+        'Average radius': 6051,8 #km
+    },
+    '3':{
+        'Name': 'Earth',
+        'Distance': 149*(10**6),
+        'Satellite': [{'Name': 'Moon'}] #{'Name':'ups'},
+        'Weight': 5,9726*(10**24) #kg
+        'The first cosmic speed': 7,91 #km/s
+        'The second cosmic speed': 11,186 #km/s
+        'Average radius': 6378,1 #km
+    },
+    '4':{
+        'Name': 'Mars',
+        'Distance': 228*(10**6),
+        'Satellite': [{'Name': 'Demos'},{'Name': 'Fobos'}] #{'Name':'ups'},
+        'Weight': 6,4185*(10**23) #kg
+        'The first cosmic speed':  #km/s
+        'The second cosmic speed': 5,03 #km/s
+        'Average radius': 3389,5 #km
+    },
+    '5':{
+        'Name': 'Jupiter',
+        'Distance': 778*(10**6),
+        'Satellite': [{'Name': 'Europe'},{'Name': 'Yo'},{'Name': 'Ganemed'},{'Name': 'Kalisto'}] #{'Name':'ups'},
+        'Weight': 1,8986*(10**27) #kg
+        'The first cosmic speed': 42,073 #km/s
+        'The second cosmic speed': 59,5  #km/s
+        'Average radius': 69 911 #km
+    },
+    '6':{
+        'Name': 'Saturn',
+        'Distance': 1430*(10**6),
+        'Satellite': [{'Name': 'Titan'},{'Name': 'Encelad'},{'Name': 'Diona'},{'Name': 'Tephia'}] #{'Name':'ups'},
+        'Weight': 5,6846*(10**26) #kg
+        'The first cosmic speed': #km/s
+        'The second cosmic speed': 35,5 #km/s
+        'Average radius': 58 232 #km
+    },
+    '7':{
+        'Name': 'Uranus',
+        'Distance': 2.8*(10**9),
+        'Satellite': [{'Name': 'Titania'},{'Name': 'Oberon'},{'Name': 'Umbrielle'},{'Name': 'Ariel'}] #{'Name':'ups'},
+        'Weight': 8,6832*(10**25) #kg
+        'The first cosmic speed': #km/s
+        'The second cosmic speed': 21,3 #km/s
+        'Average radius': 25 362 #km
+    },
+    '8':{
+        'Name': 'Neptun',
+        'Distance': 4.55*(10**9),
+        'Satellite': [{'Name': 'Triton'},{'Name': 'Protey'},{'Name': 'Nereida'},{'Name': 'Larissa '}] #{'Name':'ups'},
+        'Weight': 1,0243*(10**26) #kg
+        'The first cosmic speed': #km/s
+        'The second cosmic speed': 23,5 #km/s
+        'Average radius': 24 622 #km
+    },
+    '9':{
+        'Name': 'Pluto',
+        'Distance': 5.96*(10**9),
+        'Satellite': [{'Name': 'Haron'},{'Name': 'Nikta'},{'Name': 'Hydra'},{'Name': 'S2011'},{'Name': 'S2012'}] #{'Name':'ups'},
+        'Weight': 1,303*(10**22)#kg
+        'The first cosmic speed': 0,855 #km/s
+        'The second cosmic speed': 1,210 #km/s
+        'Average radius': 1187 #km
+    },
+}
