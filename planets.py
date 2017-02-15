2f30b691d18aae32fe8f08479ef40d4f4139ed9b
Daniil Marynich <bandydan@gmail.com>
2017-02-15 21:31:28 +0200

    fixed some issues

--- planets.py
+++ planets.py
@@ -2,19 +2,11 @@ planets = {
     '1': {
         'Name': ' Mercury ',
         'Distance': 58 * (10 ** 6),
-<<<<<<< HEAD:planets1.py
-        'Satellite': [],                      # {'Name':'ups'},
-        ' Weight ': 3.33022 * (10 ** 23),     # kg
-        'The first cosmic speed': 3.1,        # km/s
-        'The second cosmic speed': 4.25,      # km/s
-        'Average radius': 2439.7              # km
-=======
         'Satellite': [],  # {'Name':'ups'} ,
         'Weight': 3.33022 * (10 ** 23),   # kg
         'The first cosmic speed': 3.1,    # km/s
         'The second cosmic speed': 4.25,  # km/s
         'Average radius': 2439.7         # km
->>>>>>> efeb4a43e21eee6fa245f03423e9ab2eea57a95f:planets.py
     },
     '2': {
         'Name': 'Venera',
@@ -37,14 +29,6 @@ planets = {
     '4': {
         'Name': ' Mars ',
         'Distance': 228 * (10 ** 6),
-<<<<<<< HEAD:planets1.py
-        'Satellite': [{' Name ': ' Demos '},
-                      {' Name ': ' Fobos '}],  # {'Name':'ups'},
-        'Weight': 6.4185 * (10 ** 23),         # kg
-        'The first cosmic speed': None,        # km/s
-        'The second cosmic speed': 5.03,       # km/s
-        'Average radius': 3389.5               # km
-=======
         'Satellite': [
             {' Name ': ' Demos '},
             {' Name ': ' Fobos '}
@@ -53,20 +37,10 @@ planets = {
         'The first cosmic speed': None,          # km/s
         'The second cosmic speed': 5.03,      # km/s
         'Average radius': 3389.5             # km
->>>>>>> efeb4a43e21eee6fa245f03423e9ab2eea57a95f:planets.py
     },
     '5': {
         'Name': ' Jupiter ',
         'Distance': 778 * (10 ** 6),
-<<<<<<< HEAD:planets1.py
-        'Satellite': [{' Name ': ' Europe '}, {' Name ': ' Yo '},
-                      {' Name ': ' Ganemed '},
-                      {' Name ': ' Kalisto '}],  # {'Name':'ups'},
-        'Weight': 1.8986 * (10 ** 27),           # kg
-        'The first cosmic speed': 42.073,        # km/s
-        'The second cosmic speed': 59.5,         # km/s
-        'Average radius': 69911                  # km
-=======
         'Satellite': [
             {' Name ': ' Europe '},
             {' Name ': ' Yo '},
@@ -77,20 +51,10 @@ planets = {
         'The first cosmic speed': 42.073,  # km/s
         'The second cosmic speed': 59.5,   # km/s
         'Average radius': 69911          # km
->>>>>>> efeb4a43e21eee6fa245f03423e9ab2eea57a95f:planets.py
     },
     '6': {
         'Name': ' Saturn ',
         'Distance': 1430 * (10 ** 6),
-<<<<<<< HEAD:planets1.py
-        'Satellite': [{' Name ': ' Titan '}, {' Name ': ' Encelad '},
-                      {' Name ': ' Diona '},
-                      {' Name ': ' Tephia '}],   # {'Name':'ups'},
-        'Weight': 5.6846 * (10 ** 26),           # kg
-        'The first cosmic speed': None,          # km/s
-        'The second cosmic speed': 35.5,         # km/s
-        'Average radius': 58232                  # km
-=======
         'Satellite': [
             {' Name ': ' Titan '},
             {' Name ': ' Encelad '},
@@ -101,20 +65,10 @@ planets = {
         'The first cosmic speed': None,     # km/s
         'The second cosmic speed': 35.5,  # km/s
         'Average radius': 58232         # km
->>>>>>> efeb4a43e21eee6fa245f03423e9ab2eea57a95f:planets.py
     },
     '7': {
         'Name': ' Uranus ',
         'Distance': 2.8 * (10 ** 9),
-<<<<<<< HEAD:planets1.py
-        'Satellite': [{' Name ': ' Titania '}, {' Name ': ' Oberon '},
-                      {' Name ': ' Umbrielle '},
-                      {' Name ': ' Ariel '}],   # {'Name':'ups'},
-        'Weight': 8.6832 * (10 ** 25),          # kg
-        'The first cosmic speed': None,         # km/s
-        'The second cosmic speed': 21.3,        # km/s
-        'Average radius': 25362                 # km
-=======
         'Satellite': [
             {' Name ': ' Titania '},
             {' Name ': ' Oberon '},
@@ -125,20 +79,10 @@ planets = {
         'The first cosmic speed': None,     # km/s
         'The second cosmic speed': 21.3,  # km/s
         'Average radius': 25362         # km
->>>>>>> efeb4a43e21eee6fa245f03423e9ab2eea57a95f:planets.py
     },
     '8': {
         'Name': ' Neptun ',
         'Distance': 4.55 * (10 ** 9),
-<<<<<<< HEAD:planets1.py
-        'Satellite': [{' Name ': ' Triton '}, {' Name ': ' Protey '},
-                      {' Name ': ' Nereida '},
-                      {' Name ': ' Larissa '}],  # {'Name':'ups'},
-        'Weight': 1.0243 * (10 ** 26),           # kg
-        'The first cosmic speed': None,          # km/s
-        'The second cosmic speed': 23.5,         # km/s
-        'Average radius': 24622                  # km
-=======
         'Satellite': [
             {' Name ': ' Triton '},
             {' Name ': ' Protey '},
@@ -149,20 +93,10 @@ planets = {
         'The first cosmic speed': None,     # km/s
         'The second cosmic speed': 23.5,   # km/s
         'Average radius': 24622          # km
->>>>>>> efeb4a43e21eee6fa245f03423e9ab2eea57a95f:planets.py
     },
     '9': {
         'Name': ' Pluto ',
         'Distance': 5.96 * (10 ** 9),
-<<<<<<< HEAD:planets1.py
-        'Satellite': [{' Name ': ' Haron '}, {' Name ': ' Nikta '},
-                      {' Name ': ' Hydra '}, {' Name ': ' S2011 '},
-                      {' Name ': ' S2012 '}],     # {'Name':'ups'},
-        'Weight': 1.303 * (10 ** 22),             # kg
-        'The first cosmic speed': 0.855,          # km/s
-        'The second cosmic speed': 1.210,         # km/s
-        'Average radius': 1187                    # km
-=======
         'Satellite': [
             {' Name ': ' Haron '},
             {' Name ': ' Nikta '},
@@ -174,7 +108,6 @@ planets = {
         'The first cosmic speed': 0.855,     # km/s
         'The second cosmic speed': 1.210,    # km/s
         'Average radius': 1187              # km
->>>>>>> efeb4a43e21eee6fa245f03423e9ab2eea57a95f:planets.py
     },
 }
 raw_input()
