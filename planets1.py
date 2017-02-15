716cc79c8c229cf2f0aafc7c409ee46cd848dacd
Tomax88 <wizik88@gmail.com>
2017-02-15 20:50:58 +0200

    new planets

--- planets1.py
+++ planets1.py
@@ -2,11 +2,11 @@ planets = {
     '1': {
         'Name': ' Mercury ',
         'Distance': 58 * (10 ** 6),
-        'Satellite': [] ,  # {'Name':'ups'} ,
-        'Weight' : 3.33022 * (10 ** 23) ,   # kg
-        'The first cosmic speed': 3.1 ,    # km/s
-        'The second cosmic speed': 4.25 ,  # km/s
-        'Average radius': 2439.7         # km
+        'Satellite': [],  # {'Name':'ups'},
+        ' Weight ': 3.33022 * (10 ** 23),   # kg
+        'The first cosmic speed': 3.1,      # km/s
+        'The second cosmic speed': 4.25,    # km/s
+        'Average radius': 2439.7            # km
     },
     '2': {
         'Name': 'Venera',
@@ -15,7 +15,7 @@ planets = {
         'Weight': 4.8675 * (10 ** 24),      # kg
         'The first cosmic speed': 7.328,    # km/s
         'The second cosmic speed': 10.363,  # km/s
-        'Average radius': 6051.8           # km
+        'Average radius': 6051.8            # km
     },
     '3': {
         'Name': ' Earth ',
@@ -29,55 +29,67 @@ planets = {
     '4': {
         'Name': ' Mars ',
         'Distance': 228 * (10 ** 6),
-        'Satellite': [{' Name ': ' Demos '}, {' Name ': ' Fobos '}], # {'Name':'ups'},
-        'Weight': 6.4185 * (10 ** 23),        # kg
-        'The first cosmic speed': None,          # km/s
-        'The second cosmic speed': 5.03,      # km/s
-        'Average radius': 3389.5             # km
+        'Satellite': [{' Name ': ' Demos '},
+                      {' Name ': ' Fobos '}],  # {'Name':'ups'},
+        'Weight': 6.4185 * (10 ** 23),         # kg
+        'The first cosmic speed': None,        # km/s
+        'The second cosmic speed': 5.03,       # km/s
+        'Average radius': 3389.5               # km
     },
     '5': {
         'Name': ' Jupiter ',
         'Distance': 778 * (10 ** 6),
-        'Satellite': [{' Name ': ' Europe '}, {' Name ': ' Yo '}, {' Name ': ' Ganemed '}, {' Name ': ' Kalisto '}],  # {'Name':'ups'},
-        'Weight': 1.8986 * (10 ** 27),     # kg
-        'The first cosmic speed': 42.073,  # km/s
-        'The second cosmic speed': 59.5,   # km/s
-        'Average radius': 69911          # km
+        'Satellite': [{' Name ': ' Europe '}, {' Name ': ' Yo '},
+                      {' Name ': ' Ganemed '},
+                      {' Name ': ' Kalisto '}],  # {'Name':'ups'},
+        'Weight': 1.8986 * (10 ** 27),           # kg
+        'The first cosmic speed': 42.073,        # km/s
+        'The second cosmic speed': 59.5,         # km/s
+        'Average radius': 69911                  # km
     },
     '6': {
         'Name': ' Saturn ',
         'Distance': 1430 * (10 ** 6),
-        'Satellite': [{' Name ': ' Titan '}, {' Name ': ' Encelad '}, {' Name ': ' Diona '}, {' Name ': ' Tephia '}],   # {'Name':'ups'},
-        'Weight': 5.6846 * (10 ** 26),    # kg
-        'The first cosmic speed': None,     # km/s
-        'The second cosmic speed': 35.5,  # km/s
-        'Average radius': 58232         # km
+        'Satellite': [{' Name ': ' Titan '}, {' Name ': ' Encelad '},
+                      {' Name ': ' Diona '},
+                      {' Name ': ' Tephia '}],   # {'Name':'ups'},
+        'Weight': 5.6846 * (10 ** 26),           # kg
+        'The first cosmic speed': None,          # km/s
+        'The second cosmic speed': 35.5,         # km/s
+        'Average radius': 58232                  # km
     },
     '7': {
         'Name': ' Uranus ',
         'Distance': 2.8 * (10 ** 9),
-        'Satellite': [{' Name ': ' Titania '}, {' Name ': ' Oberon '}, {' Name ': ' Umbrielle '}, {' Name ': ' Ariel '}],  # {'Name':'ups'},
-        'Weight': 8.6832 * (10 ** 25),    # kg
-        'The first cosmic speed': None,     # km/s
-        'The second cosmic speed': 21.3,  # km/s
-        'Average radius': 25362         # km
+        'Satellite': [{' Name ': ' Titania '}, {' Name ': ' Oberon '},
+                      {' Name ': ' Umbrielle '},
+                      {' Name ': ' Ariel '}],   # {'Name':'ups'},
+        'Weight': 8.6832 * (10 ** 25),          # kg
+        'The first cosmic speed': None,         # km/s
+        'The second cosmic speed': 21.3,        # km/s
+        'Average radius': 25362                 # km
     },
     '8': {
         'Name': ' Neptun ',
         'Distance': 4.55 * (10 ** 9),
-        'Satellite': [{' Name ': ' Triton '}, {' Name ': ' Protey '}, {' Name ': ' Nereida '}, {' Name ': ' Larissa '}],  # {'Name':'ups'},
-        'Weight': 1.0243 * (10 ** 26),     # kg
-        'The first cosmic speed': None   ,     # km/s
-        'The second cosmic speed': 23.5,   # km/s
-        'Average radius': 24622          # km
+        'Satellite': [{' Name ': ' Triton '}, {' Name ': ' Protey '},
+                      {' Name ': ' Nereida '},
+                      {' Name ': ' Larissa '}],  # {'Name':'ups'},
+        'Weight': 1.0243 * (10 ** 26),           # kg
+        'The first cosmic speed': None,          # km/s
+        'The second cosmic speed': 23.5,         # km/s
+        'Average radius': 24622                  # km
     },
     '9': {
         'Name': ' Pluto ',
         'Distance': 5.96 * (10 ** 9),
-        'Satellite': [{' Name ': ' Haron '}, {' Name ': ' Nikta '}, {' Name ': ' Hydra '}, {' Name ': ' S2011 '}, {' Name ': ' S2012 '}],   # {'Name':'ups'},
-        'Weight': 1.303 * (10 ** 22),        # kg
-        'The first cosmic speed': 0.855,     # km/s
-        'The second cosmic speed': 1.210,    # km/s
-        'Average radius': 1187              # km
+        'Satellite': [{' Name ': ' Haron '}, {' Name ': ' Nikta '},
+                      {' Name ': ' Hydra '}, {' Name ': ' S2011 '},
+                      {' Name ': ' S2012 '}],     # {'Name':'ups'},
+        'Weight': 1.303 * (10 ** 22),             # kg
+        'The first cosmic speed': 0.855,          # km/s
+        'The second cosmic speed': 1.210,         # km/s
+        'Average radius': 1187                    # km
     },
 }
+raw_input()
