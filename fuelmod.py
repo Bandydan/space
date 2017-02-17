6641ff9016d4851f621c6c4ed5e02f3e72780647
13Ustas <georg.svit@gmail.com>
2017-02-17 10:02:53 +0200

    File with fuel data

---
+++ fuelmod.py
@@ -0,0 +1,32 @@
+fuel = {
+    '1': {
+        ' Name ': 'Kerosene + liquid oxygen',
+        ' Mass ': None,
+        ' Specific impulse ': 358,    # m/s
+        ' Density ': 1.036,           # g/sm^3
+    },
+    '2': {
+        ' Name ': 'Oxygen + hydrogen',
+        ' Mass ': None,
+        ' Specific impulse ': 455,    # m/s
+        ' Density ': 0.3155,          # g/sm^3
+    },
+    '3': {
+        ' Name ': 'Asymmetric dimethylhydrazine + nitrogen tetroxide',
+        ' Mass ': None,
+        ' Specific impulse ': 344,    # m/s
+        ' Density ': 1.185,           # g/sm^3
+    },
+    '4': {
+        ' Name ': ' Ammonium perchlorate ',
+        ' Mass ': None,
+        ' Specific impulse ': 250,    # m/s
+        ' Density ': 1.95,            # g/sm^3
+    },
+    '5': {
+        ' Name ': ' None ',
+        ' Mass ': None,
+        ' Specific impulse ': None,   # m/s
+        ' Density ': None,            # g/sm^3
+    }
+}
